faa37e6e729f6ace5b841b4e0c3a3db902f0fd9d
naveen <naveen.panwar27@gmail.com>
2016-06-25 07:40:39 +0530

    Finished Getting Started

--- ror-noob/blog/config/routes.rb
+++ ror-noob/blog/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   resources :articles do
-    resource :comments
+    resources :comments
   end
 
   root 'welcome#index'
