6f96c164c9bf6a337a4b4592607c5e8e6b03994c
naveen <naveen.panwar27@gmail.com>
2016-06-29 13:12:43 +0530

    stalling pubnub due to insufficient knowledge

--- js-noob/pubnub/chat_controller.js
+++ js-noob/pubnub/chat_controller.js
@@ -9,3 +9,45 @@ angular.module('app', ["pubnub.angular.service"])
          uuid: $scope.uuid
        });
 });
+
+// Send the messages over PubNub Network
+$scope.sendMessage = function() {
+   // Don't send an empty message 
+   if (!$scope.messageContent || $scope.messageContent === '') {
+        return;
+    }
+    Pubnub.publish({
+        channel: $scope.channel,
+        message: {
+            content: $scope.messageContent,
+            sender_uuid: $scope.uuid,
+            date: new Date()
+        },
+        callback: function(m) {
+            console.log(m);
+        }
+    });
+    // Reset the messageContent input
+    $scope.messageContent = '';
+
+}
+
+$scope.messages = [];
+
+// Subscribing to the ‘messages-channel’ and trigering the message callback
+Pubnub.subscribe({
+    channel: $scope.channel,
+    triggerEvents: ['callback']
+});
+
+// Listening to the callbacks
+$scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
+    $scope.$apply(function () {
+        $scope.messages.push(m)
+    });
+});
+
+// A function to display a nice uniq robot avatar 
+$scope.avatarUrl = function(uuid){
+    return 'http://robohash.org/'+uuid+'?set=set2&bgset=bg2&size=70x70';
+};
