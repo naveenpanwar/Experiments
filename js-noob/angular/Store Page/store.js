78d2192bb23944b2c2f390a9958ecd5c220c174d
Naveen Panwar <naveen.panwar27@gmail.com>
2017-09-21 15:34:03 +0530

    reached the angular and grids stuff

--- js-noob/angular/Store Page/store.js
+++ js-noob/angular/Store Page/store.js
@@ -1,1 +1,16 @@
-var app = angular.module("ShopsApp", [] );
+var shopsApp = angular.module("ShopsApp", [] );
+
+shopsApp.controller("ShopsController", function CountryListController( $scope, $http ) {
+  $http.get("./shops.json").
+    success( function(data, status, headers, config) {
+      $scope.countries = data;
+    }).
+    error( function( data, status, headers, config) {
+      console.log("Cannot parse Json");
+    });
+});
+
+function show_shops(elmt) {
+  console.log(elmt.innerHTML);
+}
+
